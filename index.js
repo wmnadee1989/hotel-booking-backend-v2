610433186a2fa667531831c079a4f4d6dfa4402d
wmnadee1989 <nadeesha@pmgs.lk>
2024-10-09 21:15:49 +0530

    add price new

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req,res,next)=>{
 
 mongoose.connect(connectionString).then(
   ()=>{
-    console.log("Connected to the database")
+    console.log("Connected to the database.")
   }
 ).catch(
   ()=>{
