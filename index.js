ce8a33f67c85a5061e73abcf03a7b3af943b4aaf
wmnadee1989 <nadeesha@pmgs.lk>
2024-10-10 10:45:07 +0530

    test12

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req,res,next)=>{
 
 mongoose.connect(connectionString).then(
   ()=>{
-    console.log("Connected to the databasetest00023")
+    console.log("Connected to the databasetest000254453")
   }
 ).catch(
   ()=>{
